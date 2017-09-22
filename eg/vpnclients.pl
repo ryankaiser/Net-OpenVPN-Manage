107f3e739798f9aa207c38f4546216a1ed32d2c3
Ryan Kaiser <ryandkaiser@gmail.com>
2017-09-21 22:49:50 -0500

    Show IPv6 client addresses

--- eg/vpnclients.pl
+++ eg/vpnclients.pl
@@ -15,8 +15,7 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.#
 
 use strict;
 use warnings;
@@ -113,56 +112,72 @@ if ( $total_clients > 0 ) {
             if (scalar(@vpns) > 1 && $vpn != $vpns[-1]) {
                 print "\n";
             }
-            printf("%2s Client List for %-s:%d\n\n","",
-                $vpn->{host}, $vpn->{port}) if ($verbose);
+            printf("%2s %sClient List for %s%-s%s:%s%d%s\n\n","",
+                "\033[01;31m", "\033[01;35m", $vpn->{host}, "\033[00m",
+                "\033[01;33m", $vpn->{port}, "\033[00m") if ($verbose);
         }
 
         my @clients;
+        my $has_ipv6 = 0;
         my $stats = $vpn->status("2");
+
+        for my $line (@$stats) {
+            if ($line =~ /Virtual IPv6 Address/) {
+                $has_ipv6 = 1;
+                last;
+            }
+        }
+
         for my $line (@$stats) {
             if ($line =~ /^CLIENT_LIST/) {
                 my @parts = split(",", $line);
+                next if $parts[1] eq "UNDEF";
                 push (@clients, {
                     cn => $parts[1],
                     ra => $parts[2],
                     va => $parts[3],
-                    bi => $parts[4],
-                    bo => $parts[5],
-                    cs => $parts[6],
+                    v6 => $has_ipv6 ? $parts[4] : undef,
+                    bi => $has_ipv6 ? $parts[5] : $parts[4],
+                    bo => $has_ipv6 ? $parts[6] : $parts[5],
+                    cs => $has_ipv6 ? $parts[7] : $parts[6],
                });
-            }            
+            }
         }
+
         for my $client (@clients) {
+
             if ($verbose) {
                 printf("%2s %-16s: %-29s\n","",
                     "Common Name", $client->{cn});
                 printf("%2s %-16s: %-29s\n","",
-                    "Virtual Address", $client->{va});
-                printf("%2s %-16s: %-29s\n","",
                     "Real Address", $client->{ra});
                 printf("%2s %-16s: %-29s\n","",
-                    "Connected Since", $client->{cs});               
+                    "Virtual Address", $client->{va});
                 printf("%2s %-16s: %-29s\n","",
-                    "Bytes In", $client->{bi});
+                    "Virtual IPv6", $client->{v6}) if $has_ipv6;
                 printf("%2s %-16s: %-29s\n","",
-                    "Bytes Out", $client->{bo});
+                    "Connected Since", $client->{cs});
+                printf("%2s %-16s: %2.2f MiB in | %2.2f MiB out\n","",
+                    "Bytes", $client->{bi}/1024/1024,$client->{bo}/1024/1024);
                 if ($vpn->{nclients} > 1 && $client != $clients[-1]) {
                     print "\n";
                 }
             }
             else {
-                printf("%-s %-s\n",
-                    $client->{va}, $client->{cn});
-            }
-        }               
-    }        
+                printf("%-s", $client->{va});
+                printf(" | %-s", $client->{v6}) unless not $has_ipv6;
+                printf(" | %-s\n", $client->{cn});
+           }
+        }
+        $vpn->{objects}{_telnet_}->close;
+    }
 }
 else {
     print "No clients are currently connected\n";
 }
 
 exit 0;
-       
+
 
 __END__
 
@@ -177,7 +192,6 @@ vpnclients [OPTION]...
 =item S<--conf-dir=PATH    OpenVPN config directory (default=/etc/openvpn)>
 
 =item S<-h, --host=NAME    Host to which to connect (default=127.0.0.1)>
-
 =item S<-p, --port=PORT    Port to which to connect (default=parse .conf[s])>
 
 =item S<-v, --verbose      Increase verbosity>
